180ea2ae00b0598ac96d6016495187844835e9bc
Jenaro Garcia <jgarc609@ford.com>
2020-07-16 07:21:38 +0100

    reverted

--- app/src/main/java/com/example/truthtable_tdd_example/MainSupport.kt
+++ app/src/main/java/com/example/truthtable_tdd_example/MainSupport.kt
@@ -1,17 +1,15 @@
 package com.example.truthtable_tdd_example
 
-import android.graphics.Color
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 
 class MainSupport{
     fun showSnackBar(message:String,view: View){
-        var snackbar: Snackbar = Snackbar.make(
+        var snackBar: Snackbar = Snackbar.make(
             view,
             message,
             Snackbar.LENGTH_LONG
         )
-        snackbar
-        snackbar.show()
+        snackBar.show()
     }
 }
--- app/src/test/java/com/example/truthtable_tdd_example/MainSupportTest.kt
+++ app/src/test/java/com/example/truthtable_tdd_example/MainSupportTest.kt
@@ -1,7 +1,22 @@
 package com.example.truthtable_tdd_example
 
-import org.junit.Assert.*
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.runs
+import io.mockk.slot
+import org.junit.Test
 
-class MainSupportTest{
+class MainSupportTest {
+    @Test
+    fun `showSnackBar- with  message and view - displays snackBar()`() {
+        val subject: MainSupport = MainSupport()
+        val message:String="test"
+        val view:View= mockk()
+        //val snackBarSlot= slot<Snackbar>()
+        every { Snackbar.make(view,message) } just runs
 
+
+    }
 }
